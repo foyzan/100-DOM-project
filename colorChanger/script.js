4f698cb0af4bc8bab681cb71cfb042508464a533
foyzan <foyzanahmedapon01@gmail.com>
2024-11-20 16:51:38 +0600

    completed

---
+++ colorChanger/script.js
@@ -0,0 +1,12 @@
+const buttons = document.querySelectorAll('.button');
+const body = document.querySelector('body');
+
+
+buttons.forEach(function(button){
+    button.addEventListener('click', function(event){
+        // body.style.backgroundColor = event.target.id;
+        let id = event.target.id
+        body.setAttribute('id', id);
+    })
+})
+
