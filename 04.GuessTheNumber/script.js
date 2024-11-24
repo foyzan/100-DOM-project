5fc948b94ddff38408eb02604f0a9813318384f3
foyzan <foyzanahmedapon01@gmail.com>
2024-11-24 13:48:39 +0600

    added some more features and newGame functions

--- 04.GuessTheNumber/script.js
+++ 04.GuessTheNumber/script.js
@@ -1,4 +1,4 @@
-const randomNumber = parseInt(Math.random() * 100 + 1);
+let randomNumber = parseInt(Math.random() * 100 + 1);
 console.log(randomNumber);
 
 const submitButton = document.querySelector("#subt");
@@ -30,9 +30,16 @@ function validateGuess(guess) {
     } else {
         prevGuess.push(guess);
         if (numGuess === 10) {
-            displayGuess(guess);
-            displayMessage(`Game Over!!  ${randomNumber}`);
-            endGame();
+            if (checkGuess(guess)) {
+                return;
+            } else {
+                displayGuess(guess);
+                const over = document.createElement("h2");
+                over.classList.add("lowOrHi");
+                over.innerHTML = `Game Over!!  ${randomNumber}`;
+                endGame();
+                document.querySelector(".button").appendChild(over);
+            }
         } else {
             displayGuess(guess);
             checkGuess(guess);
@@ -43,8 +50,12 @@ function validateGuess(guess) {
 function checkGuess(guess) {
     //
     if (guess === randomNumber) {
-        displayMessage(`You Won!! ${randomNumber}`);
+        const win = document.createElement("h2");
+        win.classList.add("lowOrHi");
+        win.innerHTML = `You Won!! ${randomNumber}`;
         endGame();
+        document.querySelector(".button").appendChild(win);
+        return true;
     } else if (Math.abs(randomNumber - guess) < 3) {
         displayMessage(`You are Too close`);
     } else if (Math.abs(randomNumber - guess) < 5) {
@@ -71,19 +82,28 @@ function displayMessage(msg) {
 
 function endGame() {
     inputBox.value = "";
-    inputBox.setAttribute("disable", "");
+    inputBox.setAttribute("disabled", "");
     div.classList.add("button");
-    div.innerHTML = `<button>Start Over</button>`; /* if I use <button><button>  it will reload the page
+    div.innerHTML = `<button id = "startNew" type="button">Start Over</button>`; /* if I use <button><button>  it will reload the page
                                                     and Don't need to use NewGame function but I will use <button type="button">Start Over</button>. for this reason I have to use newGame function*/
-    startOver.appendChild(div)
-    startOver.style.display = 'block';
+    startOver.appendChild(div);
+    startOver.style.display = "block";
     playGame = false;
-    
+    newGame()
 }
 
-function newGame() {
-
+function newGame() { 
+    newGameButton = document.querySelector("#startNew")
+    newGameButton.addEventListener('click', function(e){
+    randomNumber = parseInt(Math.random() * 100 + 1);
+    console.log(randomNumber);
+    prevGuess = []
+    numGuess = 1;
+    guessSlot.innerHTML = '';
+    remaining.innerHTML = `${11 - numGuess}`;
+    inputBox.removeAttribute('disabled')
+    startOver.removeChild(div)
+    startOver.style.display = "none";
+    playGame = true;
+    })
 }
-
-
-    
